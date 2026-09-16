7f5bcb784a93bcf1981aa7e0b7220ffcef16e629
DiegoProjetoIntegrador7 <diegoprojetointegrador@gmail.com>
2026-09-16 01:36:54 +0000

    ex6

--- projetoum/src/app/lista-exercicio2/exercicio6/exercicio6.ts
+++ projetoum/src/app/lista-exercicio2/exercicio6/exercicio6.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './exercicio6.html',
   styleUrl: './exercicio6.css',
 })
-export class Exercicio6 {}
+export class Exercicio6 {
+  nomes = ['Diego', 'Bruno', 'Carlos', 'Davi', 'Miguel'];
+
+  removerUltimo() {
+    this.nomes.length = this.nomes.length - 1;
+  }
+
+  limparLista() {
+    this.nomes = [];
+  }
+
+  restaurarLista() {
+    this.nomes = ['Diego', 'Bruno', 'Carlos', 'Davi', 'Miguel'];
+  }
+}
