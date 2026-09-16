ad976831e4be78dfdc8e478b9bf3336d2a40d608
DiegoProjetoIntegrador7 <diegoprojetointegrador@gmail.com>
2026-09-16 01:28:16 +0000

    ex5

--- projetoum/src/app/lista-exercicio2/exercicio5/exercicio5.ts
+++ projetoum/src/app/lista-exercicio2/exercicio5/exercicio5.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './exercicio5.html',
   styleUrl: './exercicio5.css',
 })
-export class Exercicio5 {}
+export class Exercicio5 {
+  nomes = ['Diego', 'Bruno', 'Carlos', 'Davi', 'Miguel'];
+}
