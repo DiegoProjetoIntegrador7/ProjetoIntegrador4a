1c56fcb3eb2c400afae4689b82f70aa86fffce57
DiegoProjetoIntegrador7 <diegoprojetointegrador@gmail.com>
2026-09-16 00:57:59 +0000

    ex2

---
+++ projetoum/src/app/lista-exercicio2/exercicio2/exercicio2.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio2',
+  standalone: false,
+  templateUrl: './exercicio2.html',
+  styleUrl: './exercicio2.css',
+})
+export class Exercicio2 {
+  usuarioLogado: boolean = false;
+
+  alternarLogin() {
+    this.usuarioLogado = !this.usuarioLogado;
+  }
+}
