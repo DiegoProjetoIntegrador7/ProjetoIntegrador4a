f4e01665c34be2a4ecf5e30a68f6469e3a600f38
DiegoProjetoIntegrador7 <diegoprojetointegrador@gmail.com>
2026-09-18 20:34:53 +0000

    ex13

--- projetoum/src/app/lista-exercicio2/exercicio13/exercicio13.ts
+++ projetoum/src/app/lista-exercicio2/exercicio13/exercicio13.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Tarefa {
+  id: number;
+  titulo: string;
+  responsavel: string;
+  prioridade: string;
+  concluida: boolean;
+}
+
 @Component({
   selector: 'app-exercicio13',
   standalone: false,
   templateUrl: './exercicio13.html',
-  styleUrl: './exercicio13.css',
+  styleUrl: './exercicio13.css'
 })
-export class Exercicio13 {}
+export class Exercicio13 {
+
+  tarefas: Tarefa[] = [
+    { id: 1, titulo: 'Estudar', responsavel: 'Diego', prioridade: 'alta', concluida: false },
+    { id: 2, titulo: 'Trabalho', responsavel: 'João', prioridade: 'média', concluida: true },
+    { id: 3, titulo: 'Exercícios', responsavel: 'Maria', prioridade: 'baixa', concluida: false },
+    { id: 4, titulo: 'Projeto', responsavel: 'Pedro', prioridade: 'alta', concluida: true },
+    { id: 5, titulo: 'Pesquisa', responsavel: 'Ana', prioridade: 'média', concluida: false },
+    { id: 6, titulo: 'Revisão', responsavel: 'Lucas', prioridade: 'baixa', concluida: false }
+  ];
+
+  alterar(tarefa: Tarefa) {
+    tarefa.concluida = !tarefa.concluida;
+  }
+
+}
